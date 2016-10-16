085a24a343de502c993f26e3ae20fdefb8c26fbc
RomeoMaryns <romeo@maryns.eu>
2016-10-16 21:25:47 +0200

    Gatewy App

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@
 set -e
 
 # Build the project and docker images
-mvn clean install
+mvn clean install 
 
 # Export the active docker machine IP
 export DOCKER_IP=$(docker-machine ip $(docker-machine active))
+
 
 # docker-machine doesn't exist in Linux, assign default ip if it's not set
 DOCKER_IP=${DOCKER_IP:-0.0.0.0}
